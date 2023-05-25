fd61d7e35886739e05a6f30e62e0d0447c255bb9
MSzoot <97002088+MSzoot@users.noreply.github.com>
2023-05-25 13:21:50 +0100

    useState hook to update images on btn click

--- src/meme.tsx
+++ src/meme.tsx
@@ -1,10 +1,15 @@
+import data from './data';
+import { useState } from 'react';
+
 const MemeForm = () => {
+  const [url, setUrl] = useState('https://i.imgflip.com/8p0a.jpg');
+  const getNewImg = () => {
+    const random = Math.floor(Math.random() * data.data.memes.length);
+    setUrl(data.data.memes[random].url);
+  };
   return (
     <main>
-      <form
-        className="mx-auto mt-20 flex w-10/12 flex-wrap justify-between"
-        action=""
-      >
+      <div className="mx-auto mt-20 flex w-10/12 flex-wrap justify-between">
         <input
           className="h-[35px] w-5/12 rounded-md border border-gray-400 px-2"
           type="text"
@@ -20,10 +25,14 @@ const MemeForm = () => {
         <button
           className="mt-10  w-full rounded-md border bg-gradient-to-r from-[#672280] to-[#A626D3] px-4 py-2 text-white"
           type="submit"
+          onClick={getNewImg}
         >
-          Get a new meme image 🖼️
+          Get a new meme image
         </button>
-      </form>
+      </div>
+      <div className="mt-10">
+        <img className="mx-auto" src={url} alt="" />
+      </div>
     </main>
   );
 };
--- src/nav.tsx
+++ src/nav.tsx
@@ -1,13 +1,13 @@
 const Nav = () => {
   return (
     <div className="flex h-[65px] w-full items-center justify-between bg-gradient-to-r from-[#672280] to-[#A626D3] px-10 text-3xl text-white">
-      <div className="flex items-center">
+      <div className="flex items-center gap-2">
         <img
           src="/public/png-transparent-redbubble-polite-cat-meme-funny-cat-meme-thumbnail Background Removed.png"
           alt="picture of meme cat"
-          className="h-12 w-12"
+          className="mb-2 h-12 w-12"
         />
-        <h1 className="font-bold">Meme Generator</h1>
+        <h1 className="mt-2 font-bold">Meme Generator</h1>
       </div>
       <a className="text-lg" href="">
         React training
